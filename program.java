4d94653c91330c8a98b546ce5f92c34f10a15912
Sumera01 <sumera.s.shaikh11@gmail.com>
2025-02-01 11:18:49 +0530

    updated program

--- program.java
+++ program.java
@@ -1,2 +1,37 @@
+import java.util.Random;
+import java.util.Scanner;
+
 public class program {
+    public static void main(String[] args)
+    {
+        Scanner in = new Scanner(System.in);
+        Random r =new Random();
+        int no = r.nextInt(100);
+        System.out.println(no);
+
+        System.out.print("Enter a number:");
+
+        int guess,count=0;
+        do
+        {
+            guess=in.nextInt();
+            count++;
+            if(guess!=no)
+            {
+                //(guess<no) ? System.out.println("Too low ! Try agian ") : System.out.println("Too low ! Try agian ");
+                if(guess>no)
+                {
+                    System.out.println("Too High Try Again");
+
+                }
+                else{
+                    System.out.println("Too Low Try Again");
+                }
+
+            }
+
+        }while(no!=guess);
+        System.out.println("Congratulations guessed right number in "+ count + " attempts");
+    }
+
 }
